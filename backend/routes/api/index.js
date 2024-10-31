77c9a3adcb1162a0ee8a553365ab2e18f2ad159f
pfelix23 <peter.felix23@gmail.com>
2024-10-31 18:03:56 -0400

    Add User login backend endpoint"

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,18 +1,28 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 // backend/routes/api/index.js
 // ...
 
 // GET /api/set-token-cookie
 const { setTokenCookie } = require('../../utils/auth.js');
+
 const { User } = require('../../db/models');
 
 const { requireAuth } = require('../../utils/auth.js');
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 router.get(
   '/require-auth',
