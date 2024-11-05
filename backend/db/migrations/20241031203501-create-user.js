1224c6324a137b3e400e97554b0800d7a57063a3
nickyiskool <nickyninja7@gmail.com>
2024-11-05 14:12:24 -0800

    Edited user migration

--- backend/db/migrations/20241031203501-create-user.js
+++ backend/db/migrations/20241031203501-create-user.js
@@ -1,10 +1,10 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
-if(process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;    // define your schema 
-  // in options object
-}
+// if(process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;    // define your schema 
+//   // in options object
+// }
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -48,6 +48,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Users';
-    return queryInterface.dropTable(options);
+    return queryInterface.dropTable('Users');
   }
 };
