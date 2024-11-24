788f5f040bdc388b23a355ee2aaacb785060df2a
pfelix23 <peter.felix23@gmail.com>
2024-11-24 17:30:31 -0500

    Phase 0 complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import './index.css';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
 
 const store = configureStore();
+
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 
 if (process.env.NODE_ENV !== 'production') {
