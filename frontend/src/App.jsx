9b407697937dccb0e5a3c2a4d0945d62b7562708
pfelix23 <peter.felix23@gmail.com>
2024-11-25 13:26:16 -0500

    Login Modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+import LoginFormPage from './components/LoginFormModal/LoginFormModal';
 import SignupFormPage from './components/SignupFormPage/SignupFormPage';
 import Navigation from './components/Navigation/Navigation';
 import * as sessionActions from './store/session';
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import './index.css';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import {Modal, ModalProvider } from './context/Modal';
 import * as sessionActions from './store/session';
 
 
@@ -23,8 +24,11 @@ if (import.meta.env.MODE !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
- </React.StrictMode>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
+  </React.StrictMode>
 );
