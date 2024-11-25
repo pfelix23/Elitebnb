99b8cd9537e48f30b8640f9182a6298eb582e064
pfelix23 <peter.felix23@gmail.com>
2024-11-25 12:46:48 -0500

    Phase 3

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,12 +1,54 @@
-import { LiaBookReaderSolid } from "react-icons/lia";
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { FaUserCircle } from 'react-icons/fa';
+import * as sessionActions from '../../store/session';
 
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
 
-const ProfileButton = () => {
+  const toggleMenu = (e) => {
+   e .stopPropagation(); 
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+  const closeMenu = (e) => {
+    if (ulRef.current && !ulRef.current.contains(e.target)) {
+      setShowMenu(false);
+    }
+  };
+
+  document.addEventListener('click', closeMenu);
+
+  return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
   return (
-    <div style={{ color: "black", fontSize: "100px" }}>
-      <LiaBookReaderSolid />
-    </div>
+    <>
+      <button onClick={toggleMenu}>
+        <FaUserCircle />
+      </button>
+      <ul className={ulClassName} ref={ulRef}> 
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
   );
-};
+}
 
-export default ProfileButton
+export default ProfileButton;
