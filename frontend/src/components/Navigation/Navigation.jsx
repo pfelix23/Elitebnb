51b805c3e1a80a7a2c192384cea34e01de61a0bb
pfelix23 <peter.felix23@gmail.com>
2024-11-25 13:40:14 -0500

    Phase 4

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -3,28 +3,35 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import OpenModalButton from '../OpenModalButton/OpenModalButton';
 import LoginFormModal from '../LoginFormModal/LoginFormModal';
+import SignupFormModal from '../SignupFormModal/SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
-  const sessionLinks = sessionUser ? (
-    <li>
+  let sessionLinks;
+  if(sessionUser) {
+    sessionLinks = (
+     <li>
       <ProfileButton user={sessionUser} />
-    </li>
-  ) : (
-    <>
+     </li>
+    );
+  } else {
+    sessionLinks = (
       <li>
         <OpenModalButton
-          buttonText="Log In"
-          modalComponent={<LoginFormModal />}
+        buttonText="Log In"
+        modalComponent={<LoginFormModal />}
+        />
+      </li>,
+      <li>
+        <OpenModalButton
+        buttonText="Sign Up"
+        modalComponent={<SignupFormModal />}
         />
       </li>
-      <li>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </li>
-    </>
-  );
+    );
+  }
 
   return (
     <ul>
